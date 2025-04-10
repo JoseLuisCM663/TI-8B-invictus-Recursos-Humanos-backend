950b3a3024c3e7ea2934c99497e613e1b1094d22
Jose Luis Campos Marquez <camposmarquesjoseluis663@gmail.com>
2025-04-09 21:16:37 -0600

    personas por tipo de sangre

--- src/crud/dashboard.py
+++ src/crud/dashboard.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from models.colaboradores import Colaborador
 from models.areas import Areas
 from models.quejas_sugerencias import QuejasSugerencias
+from sqlalchemy import text
 
 def obtener_totales_dashboard(db: Session):
     total_colaboradores = db.query(Colaborador).count()
@@ -13,3 +14,12 @@ def obtener_totales_dashboard(db: Session):
         "total_areas": total_areas,
         "total_quejas": total_quejas
     }
+
+
+def personas_tipo_sangre(db: Session):
+    """
+    Obtener el total de personas por tipo de sangre.
+    """
+    query = text("SELECT * FROM gym_system.vw_personas_por_tiposangre;")
+    result = db.execute(query)
+    return result.fetchall()
--- src/routers/dashboard.py
+++ src/routers/dashboard.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from config.jwt import obtener_usuario_actual
 from models.usuarios import Usuario
 from database import get_db
-from crud.dashboard import obtener_totales_dashboard
+from crud.dashboard import obtener_totales_dashboard, personas_tipo_sangre  # importar tu función
 
 router = APIRouter()
 
@@ -14,5 +14,15 @@ def get_dashboard_totals(
 ):
     try:
         return obtener_totales_dashboard(db)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/personas-tipo-sangre")
+def get_personas_por_tipo_sangre(
+    db: Session = Depends(get_db),
+    usuario: Usuario = Depends(obtener_usuario_actual)
+):
+    try:
+        return personas_tipo_sangre(db)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
